efbc4ed432f7b9f3744c3bbe57b01e502ff861a4
Yan BUATOIS <dracoctix@outlook.fr>
2019-02-01 13:00:35 +0100

    Modification de la conf.

--- src/config.js
+++ src/config.js
@@ -4,6 +4,7 @@ const CONFIG = {};
 
 CONFIG.app = process.env.APP ?? 'dev';
 CONFIG.port = process.env.PORT ?? '3000';
+CONFIG.host = process.env.HOST ?? 'localhost';
 CONFIG.db_host = process.env.DB_HOST ?? 'localhost';
 CONFIG.db_port = process.env.DB_PORT ?? '27017';
 CONFIG.db_name = process.env.DB_NAME ?? 'temoins²';
